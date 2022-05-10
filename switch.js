51df85e1a44d4c2499362cd891447555953b3b38
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-05-10 15:55:35 -0300

    end of the basic class of JS

---
+++ switch.js
@@ -0,0 +1,14 @@
+let nome = 'Arthur';
+
+switch (nome) {
+  case 'Arthur':
+    console.log('Maneiro é o Arthur');
+    break;
+
+  case 'candido':
+    console.log('Maneiro é o Arthur');
+    break;
+  default:
+    console.log('Não é o Arthur');
+    break;
+}
